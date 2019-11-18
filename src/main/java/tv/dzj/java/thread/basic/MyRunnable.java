329afa0be3b48ad3ad313a37f50314de4701da5e
daizengjie <daizengjie@gmail.com>
2019-11-18 15:57:38 +0800

    thread basics

--- src/main/java/tv/dzj/java/thread/basic/MyRunnable.java
+++ src/main/java/tv/dzj/java/thread/basic/MyRunnable.java
@@ -1,17 +1,27 @@
 package tv.dzj.java.thread.basic;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @Author: <a href="daizengjie@gmail.com">dzj</a>
  */
 public class MyRunnable implements Runnable{
     public void run() {
         //...
+
+        //当前线程已经完成了生命周期中最重要的部分，可以切换给其它线程来执行，该方法只是对线程调度器的一个建议
+        Thread.yield();
+
+        //...
     }
 
     public static void main(String[] args){
-        MyRunnable mr = new MyRunnable();
-        //最好使用线程池
-        Thread thread = new Thread(mr);
-        thread.start();
+        //最好使用手动创建线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(new MyRunnable());
+        }
+        executorService.shutdown();
     }
 }

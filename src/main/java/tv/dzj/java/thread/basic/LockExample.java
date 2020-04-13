ee6903ba240e84cae2c85197b396005b9921b9cc
daizengjie <daizengjie@gmail.com>
2020-04-13 17:49:29 +0800

    死锁

--- src/main/java/tv/dzj/java/thread/basic/LockExample.java
+++ src/main/java/tv/dzj/java/thread/basic/LockExample.java
@@ -1,5 +1,7 @@
 package tv.dzj.java.thread.basic;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author dzj
  * @date 2020/4/13 17:39
@@ -10,6 +12,33 @@ public class LockExample {
     }
 
     private static void deadLock() {
-
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+        new Thread(() -> {
+            synchronized (lock1){
+                System.out.println("获取 lock1 成功");
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2){
+                    System.out.println(Thread.currentThread().getName());
+                }
+            }
+        }).start();
+        new Thread(()->{
+            synchronized (lock2){
+                System.out.println("获取 lock2 成功");
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock1){
+                    System.out.println(Thread.currentThread().getName());
+                }
+            }
+        }).start();
     }
 }
